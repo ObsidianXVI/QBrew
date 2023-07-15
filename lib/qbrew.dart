b22c4bedecef517dac2890ad12fc69d9e883d8a7
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-07-16 06:47:07 +0800

    add epsilon greedy action selection policy

--- lib/qbrew.dart
+++ lib/qbrew.dart
@@ -7,6 +7,11 @@ part './qvector.dart';
 part './action.dart';
 part './environment.dart';
 
+enum ActionSelectionPolicy {
+  epsilonGreedy,
+  softMax,
+}
+
 void main(List<String> args) {
   final QLAgent agent = QLAgent(env: Environment(), qTable: {});
   for (int j = 0; j < 50; j++) {
@@ -20,6 +25,10 @@ void main(List<String> args) {
 class QLAgent {
   final Map<QVector, double> qTable;
   final Environment env;
+  final Random _random = Random();
+  static const double epsilon = 0.2;
+  static const double gamma = 0.9;
+  static const double alpha = 0.2;
 
   QLAgent({
     required this.env,
@@ -39,26 +48,45 @@ class QLAgent {
     }
 
     // select action using policy
-    final Action selectedAction = optimalQVector.key.action;
+    QVector selectAction(ActionSelectionPolicy policy) {
+      if (policy == ActionSelectionPolicy.epsilonGreedy) {
+        // epsilon cannot be const when it is decayed
+        final double randNum = _random.nextDouble();
+        if (randNum < epsilon) {
+          // find and perform a random action ("exploration")
+          final int randIndex = _random.nextInt(qValuesOfState.length);
+          return qValuesOfState.keys.elementAt(randIndex);
+        } else {
+          // perform the action currently known to be optimal ("exploitation")
+          return optimalQVector.key;
+        }
+      } else {
+        return optimalQVector.key;
+      }
+    }
+
+    final QVector selectedQVector =
+        selectAction(ActionSelectionPolicy.epsilonGreedy);
+    final Action selectedAction = selectedQVector.action;
 
     // perform action
     final double reward = env.computeReward(selectedAction);
     print(
-        '${env.timestep} | -> C:${env.customers} P:${env.drinkPrice} [${selectedAction.priceChange}] (${optimalQVector.value})');
+        '${env.timestep} | -> C:${env.customers} P:${env.drinkPrice} [${qValuesOfState[selectedQVector]}] (${optimalQVector.value})');
     env.timestep += 1;
     final State newState = State(
       currentPrice: env.drinkPrice,
       customers: env.customers,
     );
     final double maxFutureValue = computeMaxFutureQValue(newState);
-    final double newQValue = reward + 0.9 * maxFutureValue;
+    final double newQValue = reward + gamma * maxFutureValue;
 
     // calculate temporal difference
     final double temporalDifference = newQValue - optimalQVector.value;
 
     // compute new Q-value
     final double updatedQValue =
-        optimalQVector.value + 0.2 * temporalDifference;
+        optimalQVector.value + alpha * temporalDifference;
 
     // update Q-value of chosen action and argset in Q-table
     print('          $updatedQValue');
